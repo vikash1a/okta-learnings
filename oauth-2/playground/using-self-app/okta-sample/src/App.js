12ca8e3cc6cd4af9a5b8a7dc043950e613ea48a8
vikash1a <vikash1a2b3c@gmail.com>
2025-07-27 15:38:04 +0530

    read user metadata api done

--- oauth-2/playground/using-self-app/okta-sample/src/App.js
+++ oauth-2/playground/using-self-app/okta-sample/src/App.js
@@ -27,12 +27,43 @@ const LogoutButton = () => {
     </button>
   );
 };
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+const Profile = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = React.useState(null);
+
+  React.useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-tt7pkiwvru6lijrd.us.auth0.com";
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: `https://${domain}/api/v2/`,
+            scope: "read:current_user",
+          },
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    if (isAuthenticated) {
+      getUserMetadata();
+    }
+  }, [getAccessTokenSilently, user?.sub, isAuthenticated]);
 
   return (
     isAuthenticated && (
@@ -40,7 +71,14 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )}
       </div>
     )
   );
 };
+
--- oauth-2/playground/using-self-app/okta-sample/src/index.js
+++ oauth-2/playground/using-self-app/okta-sample/src/index.js
@@ -12,7 +12,9 @@ root.render(
       domain="dev-tt7pkiwvru6lijrd.us.auth0.com"
       clientId="IAMA2qyAF13uW5XzLoYpP6KOyznEwvGp"
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
+        audience: "https://dev-tt7pkiwvru6lijrd.us.auth0.com/api/v2/",
+        scope: "read:current_user update:current_user_metadata"
       }}
     >
       <App />
